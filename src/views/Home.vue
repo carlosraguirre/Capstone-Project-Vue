1d6aaf408a556220ed251e3d8290ff9412f6c49b
Carlos Aguirre <craguirre952@gmail.com>
2021-08-20 15:30:37 -0400

    add portfolio and modal 1 to home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,65 @@
 <template>
-  <div class="home">
-    <h1>{{ message }}</h1>
-    <div><p>List Name: <input type="text" v-model="newListName.list_name"></p>
-    <button v-on:click="createListName()"> Create new list</button>
+  <div>
+    <!-- Portfolio Section-->
+    <section class="page-section portfolio" id="portfolio">
+        <div class="container">
+            <!-- Portfolio Section Heading-->
+            <h2 class="page-section-heading text-center text-uppercase text-secondary mb-0">Your Lists</h2>
+            <!-- Icon Divider-->
+            <div class="divider-custom">
+                <div class="divider-custom-line"></div>
+                <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+                <div class="divider-custom-line"></div>
+            </div>
+            <!-- Portfolio Grid Items-->
+            <div class="row justify-content-center">
+                <!-- Portfolio Item 1-->
+                <div v-for="list in listNames" class="col-md-6 col-lg-4 mb-5">
+                   <div class ="container">{{ list.list_name  }}</div>
+                      <div class="portfolio-item mx-auto" data-bs-toggle="modal" data-bs-target="#portfolioModal1">
+                          <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
+                              <div class="portfolio-item-caption-content text-center text-white"><i class="fas fa-plus fa-3x"></i></div>
+                          </div>
+                          <img class="img-fluid" src="assets/img/portfolio/cabin.png" alt="..." />
+                      </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Portfolio Modals-->
+    <!-- Portfolio Modal 1-->
+    <div class="portfolio-modal modal fade" id="portfolioModal1" tabindex="-1" aria-labelledby="portfolioModal1" aria-hidden="true">
+        <div class="modal-dialog modal-xl">
+            <div class="modal-content">
+                <div class="modal-header border-0"><button class="btn-close" type="button" data-bs-dismiss="modal" aria-label="Close"></button></div>
+                <div class="modal-body text-center pb-5">
+                    <div class="container">
+                        <div class="row justify-content-center">
+                            <div class="col-lg-8">
+                                <!-- Portfolio Modal - Title-->
+                                <h2 class="portfolio-modal-title text-secondary text-uppercase mb-0">Log Cabin</h2>
+                                <!-- Icon Divider-->
+                                <div class="divider-custom">
+                                    <div class="divider-custom-line"></div>
+                                    <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+                                    <div class="divider-custom-line"></div>
+                                </div>
+                                <!-- Portfolio Modal - Image-->
+                                <img class="img-fluid rounded mb-5" src="assets/img/portfolio/cabin.png" alt="..." />
+                                <!-- Portfolio Modal - Text-->
+                                <p class="mb-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia neque assumenda ipsam nihil, molestias magnam, recusandae quos quis inventore quisquam velit asperiores, vitae? Reprehenderit soluta, eos quod consequuntur itaque. Nam.</p>
+                                <button class="btn btn-primary" href="#!" data-bs-dismiss="modal">
+                                    <i class="fas fa-times fa-fw"></i>
+                                    Close Window
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-    <br>
-    <hr />     
-    <div v-for="list in listNames">
-      <p>{{ list.list_name }}</p>
-      <button v-on:click="goToShowRestaurantsList(list)"> See restaurants in collection</button>
-    </div>
-    <br>
-    <hr />
-  </div>
+ </div>   
 </template>
 
 <style></style>
@@ -25,10 +72,12 @@
         message: "YummyList",
         newListName: {},
         listNames: [],
+        restaurantLists: {},        
       }
     },
     created: function () {
       this.indexListNames();
+      this.indexRestaurantLists();      
     },
     methods: {
       createListName: function () {
@@ -47,7 +96,14 @@
       },
       goToShowRestaurantsList: function (list) {
         this.$router.push("/list_names/" + list.id);
-      }      
+      } ,
+      indexRestaurantLists: function () {
+        console.log("indexing restaurant lists")
+        axios.get("/list_names/" + this.$route.params.id).then(response => {
+          console.log(response.data);
+          this.restaurantLists = response.data.restaurants
+        });
+      },           
     }
   };
 </script>
